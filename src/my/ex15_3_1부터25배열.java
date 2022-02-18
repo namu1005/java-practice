2b2558caf2abd4dcf593959b360b1e445e6798b1
208726 <208726@KASH>
2022-02-18 18:45:25 +0900

    my_삭제

--- src/my/ex15_3_1부터25배열.java
+++
@@ -1,9 +0,0 @@
-package my;
-
-public class ex15_3_1부터25배열 {
-
-	public static void main(String[] args) {
-
-	}
-
-}
--- src/my/ex17_1_isDivide.java
+++
@@ -1,9 +0,0 @@
-package my;
-public class ex17_1_isDivide {
-	public static void main(String[] args) {
-		
-		
-		
-	}
-
-}
