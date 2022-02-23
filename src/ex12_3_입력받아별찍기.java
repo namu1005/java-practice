7e18d46c36d0a5641506581ef9b9cd41334d8492
namu1005 <3690cm@naver.com>
2022-02-23 22:37:25 +0900

    220223 practice

--- src/ex12_3_입력받아별찍기.java
+++ src/ex12_3_입력받아별찍기.java
@@ -1,18 +1,17 @@
 import java.util.Scanner;
 public class ex12_3_입력받아별찍기 {
 	public static void main(String[] args) {
-
+		
 		Scanner sc = new Scanner(System.in);
-		System.out.print("행 개수 : ");
+		System.out.print("행개수 : ");
 		int num = sc.nextInt();
 		
-		for (int a = 0; a < num; a++) {
-			for (int i = 0; i < a; i++) {
+		for(int i = 1; i<=num; i++) {
+			for(int a = 1; a <= i; a++) {
 				System.out.print("*");
 			}
-			System.out.println(" ");
+			System.out.println("");
 		}
-		
 
 	}
 
